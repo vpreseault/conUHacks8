bb9b1c75b5f07cf828b7d0a374671c5b0d758fcc
vpreaseault <vincent.preseault@gmail.com>
2024-01-21 00:00:50 -0500

    refactor

--- simulation/Simulation.py
+++ simulation/Simulation.py
@@ -11,24 +11,19 @@ class Simulation:
     def run(self):
         for i in range(7):
             for j in range(24):
+                num_players = utils.player_distribution[i][j]        
+                players_per_tick = round(num_players / self.ticks) 
                 for k in range(self.ticks):
-                    self.tick()
+                    for l in range(players_per_tick):
+                        day = utils.days[i]
+                        hour = utils.hours[j]
+                        p = Player(day, hour)
+
+                        self.store[p.get_region()][p.get_platform()][p.get_skill()].append(p)
+                    
+                    self.matchmaking()
         print("done")
-
-    def tick(self):
-        num_players = utils.player_distribution[i][j]        
-        players_per_tick = round(num_players / self.ticks)
-
-        self.create_players(players_per_tick)
-        self.matchmaking()
 
     def matchmaking(self):
         pass
 
-    def create_players(self, amount):
-        for l in range(amount):
-            day = utils.days[i]
-            hour = utils.hours[j]
-            p = Player(day, hour)
-
-            self.store[p.get_region()][p.get_platform()][p.get_skill()].append(p)
