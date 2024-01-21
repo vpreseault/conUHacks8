1f3d1fd3f82cd5d4d4bebf9df6b917a07e2f1fc1
vpreaseault <vincent.preseault@gmail.com>
2024-01-20 23:53:43 -0500

    refactor

--- simulation/Simulation.py
+++ simulation/Simulation.py
@@ -1,39 +1,34 @@
 import utils
 from Player import Player
 
+
 class Simulation:
     def __init__(self):
         self.players = []
         self.store = utils.dict_skeleton
+        self.ticks = 3600
 
     def run(self):
-        ticks = 3600
         for i in range(7):
             for j in range(24):
-                num_players = utils.player_distribution[i][j]
-                players_per_tick = round(num_players / ticks)
-                for k in range(ticks):
-                    for l in range(players_per_tick):
-                        day = utils.days[i]
-                        hour = utils.hours[j]
-                        p = Player(day, hour)
-
-                        self.store[p.get_region()][p.get_platform()][p.get_skill()].append(p)
-        print('done')
+                for k in range(self.ticks):
+                    self.tick()
+        print("done")
 
     def tick(self):
-        # create_players()
-        # matchmaking()
-        pass
+        num_players = utils.player_distribution[i][j]        
+        players_per_tick = round(num_players / self.ticks)
+
+        self.create_players(players_per_tick)
+        self.matchmaking()
 
     def matchmaking(self):
         pass
 
+    def create_players(self, amount):
+        for l in range(amount):
+            day = utils.days[i]
+            hour = utils.hours[j]
+            p = Player(day, hour)
 
-    def create_players(self):
-        pass
-        # is party
-
-        # party size
-
-        # create players
+            self.store[p.get_region()][p.get_platform()][p.get_skill()].append(p)
